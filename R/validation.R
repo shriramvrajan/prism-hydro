07e46f3709fde034d0ba39043641506eb7cc4be4
shriram <shriramvrajan@gmail.com>
2020-11-26 11:44:39 -0500

    Add within-basis analysis and fix code for Fig3

--- R/validation.R
+++ R/validation.R
@@ -24,6 +24,7 @@ outliers <- c(11, 564, 255, 190, 403, 393, 530)
 
 wshed <- shapefile("data/wshed_regionalized.shp")
 upst <- readRDS("data/upstream.rds")
+downst <- readRDS("data/downstream.rds")
 
 alphas_x <- readRDS("data/optim_alpha_wet_month.RDS")
 # alpha values for wet/dry interpolation
@@ -117,7 +118,7 @@ validate <- function(v, nfl = 3, by = 'subm', type='mean', ...) {
   print(NSE(q2$qs, q2$qo))
   print(paste("Pbias", pbias(q2$qs, q2$qo)))
   plot(q2$qo, q2$qs, pch=19, ...)
-  
+  return(q2)
   
 }
 
@@ -128,45 +129,58 @@ md2 <- match_day(r2)
 md3 <- match_day(r3)
 
 {
-png(filename="plots/fig3.png", width=800, height=800, 
-    pointsize=20, units="px")
-par(mfrow = c(3,3))
-cex1 = 0.5
-
-# mean
-v1 <- validate(md1, cex=cex1, main='Mean discharge,\n NN model')
-v2 <- validate(md2, cex=cex1, main='Mean discharge,\n IWGEN model')
-v3 <- validate(md3, cex=cex1, main='Mean discharge,\n RDW model')
-
-# sd
-v1s <- validate(md1, cex=cex1, type='sd', main='SD discharge,\n NN model')
-v2s <- validate(md2, cex=cex1, type='sd', main='SD discharge,\n IWGEN model')
-v3s <- validate(md3, cex=cex1, type='sd', main='SD discharge,\n RDW model')
-
-# max
-nfl1 = 3 # top n max days
-v1e <- validate(md1, cex=cex1, type='fl', nfl=nfl1,
-                main='Max. discharge,\n NN model')
-v2e <- validate(md2, cex=cex1, type='fl', nfl=nfl1,
-                main='Max. discharge,\n IWGEN model')
-v3e <- validate(md3, cex=cex1, type='fl', nfl=nfl1,
-                main='Max. discharge,\n RDW model')
-dev.off()
+  png(filename="plots/fig3.png", width=800, height=800, 
+      pointsize=20, units="px")
+  par(mfrow = c(3,3))
+  cex1 = 0.5
+  
+  # mean
+  v1 <- validate(md1, cex=cex1, main='Mean discharge,\n NN model')
+  v2 <- validate(md2, cex=cex1, main='Mean discharge,\n IWGEN model')
+  v3 <- validate(md3, cex=cex1, main='Mean discharge,\n RDW model')
+  
+  # sd
+  v1s <- validate(md1, cex=cex1, type='sd', main='SD discharge,\n NN model')
+  v2s <- validate(md2, cex=cex1, type='sd', main='SD discharge,\n IWGEN model')
+  v3s <- validate(md3, cex=cex1, type='sd', main='SD discharge,\n RDW model')
+  
+  # max
+  nfl1 = 3 # top n max days
+  v1e <- validate(md1, cex=cex1, type='fl', nfl=nfl1,
+                  main='Max. discharge,\n NN model')
+  v2e <- validate(md2, cex=cex1, type='fl', nfl=nfl1,
+                  main='Max. discharge,\n IWGEN model')
+  v3e <- validate(md3, cex=cex1, type='fl', nfl=nfl1,
+                  main='Max. discharge,\n RDW model')
+  dev.off()
 }
 
 
 ## Alphas ======================================================================
 
 {
+  adf <- data.frame(region = unlist(lapply(1:6, function(x) rep(x, 12))),
+                    month = rep(1:12, 6),
+                    x = unlist(alphas_x), 
+                    q = unlist(alphas_q))   # data frame of alphas
+  idf <- adf[, 1:2]
+  idf$I <- unlist(lapply(1:nrow(idf), function(x) {
+    cat(idf$region[x], idf$month[x], "\n")
+    p90 <- pcp90[pcp90$region == idf$region[x] & pcp90$mon == idf$month[x], ]
+    return(sd(p90$obs))
+  }))
+  idf <- tapply(idf$I, list(idf$region, idf$mon), function(x) x)
   pal <- brewer.pal(6, 'Accent')
   
-  png(filename="plots/fig6.png", width=500, height=600,
-      pointsize=10, units="px")
+  png(filename="plots/fig5.png", width=450, height=800,
+      pointsize=15, units="px")
   par(mfrow=c(2,1))
   for(i in 1:6) {
     if(i == 1) {
       plot(1:12, adf$x[adf$region == i], type='l', col=pal[i], 
-           lwd = 2, ylim = c(0, 16))
+           lwd = 2, ylim = c(0, 16), xlab = "Month", 
+           main = "Strength of interpolation parameters\n by region and month",
+           ylab = expression(paste(alpha, " for interpolating occurrence")))
     } else {
       lines(1:12, adf$x[adf$region == i], col=pal[i], lwd=2) 
     }
@@ -175,7 +189,8 @@ dev.off()
   for(i in 1:6) {
     if(i == 1) {
       plot(1:12, adf$q[adf$region == i], type='l', col=pal[i], 
-           lwd = 2, ylim = c(0, 11))
+           lwd = 2, ylim = c(0, 11), xlab = "Month",
+           ylab = expression(paste(alpha, " for interpolating quantity")))
     } else {
       lines(1:12, adf$q[adf$region == i], col=pal[i], lwd=2) 
     }
@@ -185,10 +200,21 @@ dev.off()
   dev.off()
 }
 
+### Within-basin analysis ======================================================
+
+b_nse <- function(v) {
+  return(by(v, v$sub, function(x) {
+    return(NSE(x$qs, x$qo))
+  }))
+}
+
+bn1 <- b_nse(v1)
+bn2 <- b_nse(v2)
+bn3 <- b_nse(v3)
+
 ### Predictors of failure ======================================================
 
 # GLM: NSE ~ upstream * elev * nsub (3 outliers)
-
 basin_nse <- by(v3, v3$sub, function(v) {
   return(NSE(v$qs, v$qo))
 })
@@ -243,119 +269,3 @@ plot(pof$nse, predict.lm(lm1), xlab = "obs", ylab = "sim")
 rsq(pof$nse, predict.lm(lm1))
 (summary(lm1))
 
-### Appendix ===================================================================
-# Snippets of code that I'm not using any more
-
-# pof <- merge(pof, wshed, by.x="sub", by.y="Subbasin")
-# pof <- pof[, c(1, 2, 5, 6, 7, 12, 13, 14, 15, 16, 22)]
-
-# for(i in 1:6) {
-#   if(i == 1) {
-#     plot(idf[i,], type='l', col=pal[i], lwd=2, 
-#          ylim=c(0, max(idf, na.rm=T) + 0.05))
-#   } else {
-#     lines(idf[i,], col=pal[i], lwd=2)
-#   }
-# }
-# abline(v = c(4, 10))
-
-# upst2 <- upst[names(ind)]
-# # plot(wshed, col='gray', border=NA)
-# # plot(wshed[wshed$Subbasin %in% aa,], col='blue', border=NA, add=T) # total basins
-# # plot(wshed[wshed$Subbasin %in% bb,], col='red', border=NA, add=T) # terminuses
-# # plot(wshed[wshed$Subbasin %in% cc,], col='yellow', add=T)
-# # 
-# # rch3 <- r3[r3$SUB %in% as.numeric(names(upst2)), ]
-# # md <- match_day(rch3)
-# # md2 <- match_day(r3)
-# # 
-# # vv <- validate(md, type='fl', nfl=3)
-# # 
-# # vv2 <- validate(match_day(r3), type='fl', nfl=3)
-
-# n <- tapply(v$Qo, tapplygrp, length)
-# 
-# # col1 <- switch(cols, 
-# #                'none' = rep('black', length(qobs)),
-# #                'mon' = unlist(lapply(1:12, function(n) {
-# #                  rep(brewer.pal(12, 'Paired')[n], nrow(qobs))
-# #                })))
-
-
-# leaps <- is_leap(simyears[1:nskip])
-# rch <- rch[((nskip * nsub * ifelse(leaps, 366, 365)) + 1):nrow(rch), ]
-# rch$YEAR <- unlist(lapply(simyears[(nskip + 1):length(simyears)], 
-#                           function(y) {
-#                             nday <- ifelse(is_leap(y), 366, 365)
-#                             rep(y, nsub * nday)
-#                           }))[1:nrow(rch)]
-
-
-# 
-# if(any(is.infinite(qsim))) {
-#   qsim[is.infinite(qsim)] <- NA  
-# }
-
-# par(mfrow=c(1,1))
-# if(group=='sub') {
-#   qobs <- rowMeans(qobs); qsim <- rowMeans(qsim)
-# }
-
-# add_months <- function(rch, simyears=2005:2015, nskip=1) {
-#   nsub <- length(unique(rch$SUB))
-#   leaps <- is_leap(simyears[1:nskip])
-#   rch <- rch[((nskip * nsub * ifelse(leaps, 366, 365)) + 1):nrow(rch),]
-# }
-
-# 
-# switch(cols,
-#        'none' = print("no legend"),
-#        'mon' = legend("bottomright", fill=brewer.pal(12, 'Paired'), bty="n",
-#                       legend=month_names),
-#        'sub' = legend())
-# { # drought
-#   
-#   rankyears <- function(v=v1, type='s') {
-#     if(any(v$sub %in% outliers)) v <- v[-which(v$sub %in% outliers), ]
-#     
-#     if(type == 'o') {
-#       sums <- tapply(v$Qo, list(v$sub, v$year), function(x) sum(x, na.rm=T))
-#     } else {
-#       sums <- tapply(v$Qs, list(v$sub, v$year), function(x) sum(x, na.rm=T))
-#     }
-#     ranks <- apply(sums, 1, function(x) {
-#       # NAs <- length(which(is.na(x)))
-#       out <- order(x, decreasing=FALSE, na.last=NA)
-#     })
-#   }
-#   
-#   rnk0 <- rankyears(md1, type='o')
-#   rnk1 <- rankyears(md1)
-#   rnk2 <- rankyears(md2)
-#   rnk3 <- rankyears(md3)
-#   # rnk4 <- rankyears(v4)
-#   
-#   whichyear <- function(sim, obs) {
-#     lowestobs <- obs[1]
-#     return(which(sim == lowestobs))
-#   }
-#   
-#   matchyears <- function(rnk) {
-#     return(mapply(whichyear, rnk, rnk0))
-#   }
-#   
-#   m1 <- matchyears(rnk1)
-#   m2 <- matchyears(rnk2)
-#   m3 <- matchyears(rnk3)
-#   # m4 <- matchyears(rnk4)
-#   
-#   par(mfrow = c(1, 1))
-#   
-#   hist(m3, col=rgb(0,0,1,0.4), breaks=10, border=NA, main = 'Simulated ranks of driest year by location')
-#   hist(m1, col=rgb(1,0,0,0.4), breaks=10, border=NA, add=T)
-#   legend(x=5, y=40, legend=c("Naive model", "Final model"), 
-#          fill=c(rgb(1,0,0,0.4), rgb(0,0,1,0.4)),
-#          border=NA,
-#          bty="n")
-#   
-# }
